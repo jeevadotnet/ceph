d2255bd03e5cf0b0b0f240bbbae1f332d682629e
jeevadotnet <jeevadotnet@gmail.com>
2023-01-20 11:13:18 +0200

    Add comments
    used ChatGPT3 to create comments

--- osd_crash_host_error_check.sh
+++ osd_crash_host_error_check.sh
@@ -1,4 +1,7 @@
+# loop through the list of crashed devices and extract the OSD devices
 for DISK in $(ceph crash ls | grep osd | awk '{print $2}'); do
+  # extract the hostname where the OSD device is located
   HOST=$(ceph device ls | grep "$DISK" | grep 'cephosd:sd' | awk '{print $2}' | cut -d ':' -f1)
+  # run the dmesg command to check for critical events on the host
   ssh "$HOST" "sudo dmesg -T | grep 'critical'"
 done
